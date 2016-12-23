31220611428af66c1a44a22db3f380d74d298265
Phil Callister <p.callister@sportradar.com>
2016-12-23 14:51:40 -0600

    Ability to load available frames

--- web/channels/symbol_channel.ex
+++ web/channels/symbol_channel.ex
@@ -5,6 +5,7 @@ defmodule TickerPhoenix.SymbolChannel do
   The symbol channel updates clients with a symbol's quote info
   """
   use Phoenix.Channel
+  alias Ticker.TimeFrame
 
   # Join example: "quote:symbol:TSLA"
   def join("quote:symbol:" <> symbol, _params, socket) do
@@ -17,6 +18,11 @@ defmodule TickerPhoenix.SymbolChannel do
     [symbol, interval] = String.split(symbol_interval, ":")
     Logger.info("Joining Frame: #{symbol} | Interval: #{interval}")
     {:ok, socket}
+  end
+
+  def handle_in("all_frames", %{"symbol" => symbol, "interval" => interval}, socket) do
+    frames = TimeFrame.get_frames(symbol, interval)
+    {:reply, {:ok, %{frames: frames}}, socket}
   end
 
 end
